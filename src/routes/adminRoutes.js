bb8a9cbe794ab4ab6dee910dba4bf90f6f6a3ad7
Seb13380 <sebcecg@gmail.com>
2025-08-14 09:45:44 +0200

    maj 14/08

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
-const menuController = require('../controllers/menuController');
 const { requireAdmin } = require('../middleware/auth');
 
 router.use(requireAdmin);
@@ -18,14 +17,5 @@ router.post('/classes', adminController.createClasse);
 
 router.get('/students', adminController.getStudentsManagement);
 router.post('/students', adminController.createStudent);
-
-// Routes pour la gestion des menus
-router.get('/menus', menuController.getAdminMenus);
-router.get('/menus/create', menuController.getCreateMenu);
-router.post('/menus/create', menuController.postCreateMenu);
-router.get('/menus/:id/edit', menuController.getEditMenu);
-router.post('/menus/:id/edit', menuController.postEditMenu);
-router.delete('/menus/:id', menuController.deleteMenu);
-router.post('/menus/deactivate-all', menuController.deactivateAllMenus);
 
 module.exports = router;
